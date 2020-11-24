29f96e585a2d4a8bcf7379d09ee18587b9c63b13
Louise Mpili <louise.mpili@uniqueheritage.fr>
2020-11-24 19:52:31 +0100

    ajout des fichiers html js et css de la page d'accueil

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,16 @@ const main = document.querySelector('#main-container')
 const url = "http://localhost:3000/api/furniture"
 
 fetch(url)
-    .then(resultat => resultat.json())
-    .then(json =>{
+    .then(resultat => resultat.json())//je récupérère l'objet json
+    .then(json =>{ 
+        // ensuite je vérifie que l'objet et bien récuperé dans la console
         console.log("En dessous, les produits (Il doit en avoir 5)")
         console.log(json)
+
+        //maintenant je crée une boucle pour parcourir l'objet json
         json.forEach(({_id, name, description, price, imageUrl}) =>{
+
+            //je commence par créer les éléments HTML
             const div = document.createElement("div")
             const h3 = document.createElement("h2")
             const h4 = document.createElement("h3")
@@ -15,7 +20,7 @@ fetch(url)
             const img = document.createElement("img")
             const lien = document.createElement("a")
             
-
+            //ici j'attribu les valeurs de l'objet json
             const nodeName = document.createTextNode (name)
             const nodePrice = document.createTextNode (price + " €")
             const nodeDescription = document.createTextNode (description)
@@ -23,7 +28,7 @@ fetch(url)
             lien.href = 'fiche-produit.html?id=' + _id;
             lien.textContent = "Voir le produit"
 
-
+            //j'ajoute les éléments que j'ai crée, au DOM (=> HTML)
             main.appendChild (div)
             div.appendChild (h3)
             div.appendChild (h4)
